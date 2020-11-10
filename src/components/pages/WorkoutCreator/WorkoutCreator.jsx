52dc4f62067dd6a50e27fb8ae578728fad66062b
Piotr Franczak <petetrainerlsf@gmail.com>
2020-11-10 19:36:01 +0100

    added some hard data to table with exercises in WorkoutCreator component, changed it css

--- src/components/pages/WorkoutCreator/WorkoutCreator.jsx
+++ src/components/pages/WorkoutCreator/WorkoutCreator.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Table, Thead, Tbody, Tr, Th, Td } from 'react-super-responsive-table'
 import 'react-super-responsive-table/dist/SuperResponsiveTableStyle.css'
+import "./WorkoutCreator.css"
 
 export default function WorkoutCreator(){
     return(
@@ -8,7 +9,7 @@ export default function WorkoutCreator(){
     <Table>
       <Thead>
         <Tr>
-          <Th>Order</Th>
+          <Th className="exercise-heading">Order</Th>
           <Th>Exercise</Th>
           <Th>Sets</Th>
           <Th>Reps</Th>
@@ -17,20 +18,29 @@ export default function WorkoutCreator(){
         </Tr>
       </Thead>
       <Tbody>
-        <Tr>
-          <Td>Tablescon</Td>
-          <Td>9 April 2019</Td>
-          <Td>East Annex</Td>
+        <Tr className="exercise-row">
+          <Td>1</Td>
+          <Td>Bench Press </Td>
+          <Td>3</Td>
+          <Td>12</Td>
+          <Td>90sec</Td>
+          <Td>70% RM</Td>
         </Tr>
-        <Tr>
-          <Td>Capstone Data</Td>
-          <Td>19 May 2019</Td>
-          <Td>205 Gorgas</Td>
+        <Tr className="exercise-row">
+          <Td>2</Td>
+          <Td>Pull ups</Td>
+          <Td>3</Td>
+          <Td>12</Td>
+          <Td>90sec</Td>
+          <Td>70% RM</Td>
         </Tr>
-        <Tr>
-          <Td>Tuscaloosa D3</Td>
-          <Td>29 June 2019</Td>
-          <Td>Github</Td>
+        <Tr className="exercise-row">
+          <Td>3</Td>
+          <Td>Crunches</Td>
+          <Td>3</Td>
+          <Td>12</Td>
+          <Td>90sec</Td>
+          <Td>70% RM</Td>
         </Tr>
       </Tbody>
     </Table>
