f20e87de3c95c9eaf0b3cdfcc6e9f8cec76b0a99
Piotr Franczak <petetrainerlsf@gmail.com>
2020-11-11 14:05:58 +0100

    update css for Join Component

--- src/components/pages/Join/Join.jsx
+++ src/components/pages/Join/Join.jsx
@@ -17,37 +17,37 @@ export default function Join(){
 
 
         <label for="username"> Username </label><br></br>
-        <input type="text" placeholder="Pick a username" name="username" id="username" value=""></input>
+        <input type="text" placeholder="Pick a username" name="username" id="username" ></input>
         
          </div>
      
          <div className="form-inputs">
          <label for="password"> Password </label><br></br>
-        <input type="password" placeholder="Choose a password" name="password" id="password" value=""></input>
+        <input type="password" placeholder="Choose a password" name="password" id="password"></input>
     
         </div>
 
         <div className="form-inputs">
 
         <label for="email"> Email </label><br></br>
-        <input type="email" placeholder="Add your email" name="email" id="email" value=""></input>
+        <input type="email" placeholder="Add your email" name="email" id="email"></input>
 
         </div>
 
         <div className="form-inputs">
         <label for="confirm-email"> Confirm Email </label><br></br>
-        <input type="email" placeholder="Confirm email" name="confirm-email" id="confirm-email" value=""></input>
+        <input type="email" placeholder="Confirm email" name="confirm-email" id="confirm-email"></input>
        
         </div>
         
         <div className="form-inputs">
         <label for="cars">Date of Birth</label><br></br>
         <input type="date" id="start" name="trip-start"
-        value="1900-01-01"
+        value="1990-01-01"
         min="1900-01-01" max="2020-01-01" />
         </div>
-        <div className="form-inputs">
-        <input type="checkbox" id="vehicle1" name="checkbox" value="Yes"></input>
+        <div className="form-inputs-checkbox">
+        <input type="checkbox" id="checkbox" name="checkbox" value="Yes"></input>
         <label for="checkbox">KEEP ME SIGN IN</label>
         
         
